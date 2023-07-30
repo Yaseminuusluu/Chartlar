485d67a0be47f57a462f68f84aecfc4f81376145
Yaseminuusluu <92802982+Yaseminuusluu@users.noreply.github.com>
2023-07-30 21:55:56 +0300

    Chartlar

---
+++ Chartlar/Chartlar/Deneme.xaml.cs
@@ -0,0 +1,53 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chartlar
+{
+    /// <summary>
+    /// Interaction logic for Deneme.xaml
+    /// </summary>
+    public partial class Deneme : Window
+    {
+        public Deneme()
+        {
+            InitializeComponent();
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title ="val1",
+                    Values = new  ChartValues<double>{5,10,15,20}
+                }
+            };
+
+            SeriesCollection.Add(new ColumnSeries
+            {
+                Title = "val2",
+                Values = new ChartValues<double>{10,15,20,25}
+            });
+
+            BarLabels = new[] { "values 1","values 2","values 3", "values 4" };
+            Formatter = value=>value.ToString();
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string [] BarLabels { get; set; }
+
+        public Func<double,string> Formatter { get; set; }
+    }
+}
---
+++ Chartlar/Chartlar/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chartlar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SeriesCollection = new SeriesCollection
+            {
+
+                new PieSeries
+                {
+                    Title="t1",
+                    Values = new ChartValues<ObservableValue>{new ObservableValue(10)},
+                    DataLabels= true
+                },
+                 new PieSeries
+                {
+                    Title="t2",
+                    Values = new ChartValues<ObservableValue>{new ObservableValue(20)},
+                    DataLabels= true
+                },
+                   new PieSeries
+                {
+                    Title="t3",
+                    Values = new ChartValues<ObservableValue>{new ObservableValue(27)},
+                    DataLabels= true
+                },
+                       new PieSeries
+                {
+                    Title="t4",
+                    Values = new ChartValues<ObservableValue>{new ObservableValue(33)},
+                    DataLabels= true
+                }
+            };
+            DataContext = this;
+        }
+        public SeriesCollection SeriesCollection{get; set;}
+
+        private void PieChart_DataClick(object sender,ChartPoint chartPoint)
+        {
+            MessageBox.Show("Current value:"+chartPoint.Y+"("+(chartPoint.Participation *100).ToString()+"%)");
+        }
+    }
+}
